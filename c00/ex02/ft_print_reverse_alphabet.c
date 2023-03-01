12efcc617ef9927eadeef69478dca85053cc811c
Lucas Paul <lpaul@c2r10p5.42lehavre.fr>
2023-03-01 16:20:19 +0100

    ex02 c fini

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lpaul <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/01 14:53:28 by lpaul             #+#    #+#             */
-/*   Updated: 2023/03/01 16:01:41 by lpaul            ###   ########.fr       */
+/*   Created: 2023/03/01 16:07:26 by lpaul             #+#    #+#             */
+/*   Updated: 2023/03/01 16:20:00 by lpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char	c;
 
-	c = 'a';
-	while (c <= 'z')
+	c = 'z';
+	while (c >= 'a')
 	{
 		write(1, &c, 1);
-		++c;
+		--c;
 	}
 }
 
 int	main(void)
 {
-	ft_print_alphabet();
+	ft_print_reverse_alphabet();
 	return (0);
 }
